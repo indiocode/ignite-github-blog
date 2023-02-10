1df8478cbb5964a64b0c148698c27a136fed8653
Jhollyfer Rodrigues <jhollyfer.fr@gmail.com>
2023-02-10 06:08:09 -0300

    :lipstick: configure theme and font styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/theme/default';
+
 export function App() {
 	return (
-		<div>
-			<h1>Welvome to Web App React Vite Boilerplate</h1>
-		</div>
+		<ThemeProvider theme={defaultTheme}>
+			<h1>Hello</h1>
+			<GlobalStyle />
+		</ThemeProvider>
 	);
 }
---
+++ src/styles/global.ts
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+    *,
+    *::before,
+    *::after{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        -webkit-tap-highlight-color: transparent;
+    }
+    
+    :focus{
+        outline: 0;
+        box-shadow: 0 0 0 1px ${({ theme }) => theme['blue']};
+    }
+    
+    body{
+        background-color: ${({ theme }) => theme['base-background']};
+        color: ${({ theme }) => theme['base-text']};
+        --webkit-font-smoothing: antialiased;
+    }
+    
+    body, input, textarea, button{
+        font: 400 1rem 'Nunito', sans-serif;
+    }
+
+    @media (max-width: 768px) {
+      html {
+        font-size: 87.5%;
+      }
+    }
+`;
