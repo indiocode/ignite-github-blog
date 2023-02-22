fc8aad351519aa948e70669f0e8a9ab847caf174
Jhollyfer Rodrigues <jhollyfer.fr@gmail.com>
2023-02-22 00:45:54 -0300

    :ambulance: render markdown

--- src/pages/Posts/index.tsx
+++ src/pages/Posts/index.tsx
@@ -1,27 +1,22 @@
 import type { ReactElement } from 'react';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 
 import { PostContainer } from './styles';
+
+const data = `
+- [ ] Task list 1
+- [ ] Pending task list 2
+- [x] Completed task list 3
+- [x] Completed task list 4 
+
+
+`;
 
 export function Posts(): ReactElement {
 	return (
 		<PostContainer>
-			<h1>Post</h1>
-
-			<h1>Post</h1>
-			<h1>Post</h1>
-
-			<h1>Post</h1>
-			<h1>Post</h1>
-			<h1>Post</h1>
-			<h1>Post</h1>
-			<h1>Post</h1>
-
-			<h1>Post</h1>
-			<h1>Post</h1>
-			<h1>Post</h1>
-			<h1>Post</h1>
-			<h1>Post</h1>
-			<h1>Post</h1>
+			<ReactMarkdown remarkPlugins={[remarkGfm]}>{data}</ReactMarkdown>
 		</PostContainer>
 	);
 }
--- src/pages/Posts/styles.ts
+++ src/pages/Posts/styles.ts
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
 
 export const PostContainer = styled.div`
-	border: 1px solid red;
 	max-width: 54rem;
 	margin: 0 auto;
 	padding: 2rem 2.5rem;
+
+	position: relative;
+	top: 12.25rem;
 `;
